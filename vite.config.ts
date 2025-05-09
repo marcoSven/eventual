0c176bdf48df1d845836291c8b0faf60eb4bdd13
marcoSven <me@marcoSven.com>
2025-05-08 17:58:00 -0800

    setup vite tests
    Signed-off-by: marcoSven <me@marcoSven.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from "vitest/config";
 import tailwindcss from "@tailwindcss/vite";
-// https://vite.dev/config/
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
+	test: {
+		globals: true,
+		environment: "jsdom",
+		setupFiles: "./src/setupTests.ts",
+	},
 });
